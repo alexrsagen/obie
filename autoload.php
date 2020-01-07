02ca6bd84a64f623d7debd8b69bfff5127d3b77d
Alexander Sagen <alexander@sagen.me>
2020-01-07 09:23:12 +0100

    Prevent directory escape in autoloader

--- autoload.php
+++ autoload.php
@@ -11,6 +11,8 @@ if (!defined('ZEROX_BASE_DIR')) {
 
 // Register class autoloader
 spl_autoload_register(function(string $name) {
+	static $base_path = ZEROX_BASE_DIR . DIRECTORY_SEPARATOR . 'inc' . DIRECTORY_SEPARATOR;
+
 	$ns_parts = explode('\\', $name);
 
 	// Only apply this autoloader to classes beginning with the namespace of this file
@@ -23,18 +25,21 @@ spl_autoload_register(function(string $name) {
 		$ns_parts[$i] = strtolower(preg_replace('/(?<!^)[A-Z]/', '_$0', $ns_parts[$i]));
 
 		// Get current path
-		$cur_path = ZEROX_BASE_DIR . DIRECTORY_SEPARATOR . 'inc' . DIRECTORY_SEPARATOR .
-			implode(DIRECTORY_SEPARATOR, array_slice($ns_parts, 1, $i));
+		$cur_path = $base_path . implode(DIRECTORY_SEPARATOR, array_slice($ns_parts, 1, $i));
 
 		// Ensure that current path exists
-		if ($i === count($ns_parts)-1) {
+		if ($i === count($ns_parts) - 1) {
 			$cur_path .= '.php';
-			if (!file_exists($cur_path) || !is_file($cur_path)) {
-				return;
-			}
-			require $cur_path;
-		} elseif (!file_exists($cur_path) || !is_dir($cur_path)) {
+		}
+		$cur_path = realpath($cur_path);
+		if ($cur_path === false || strncmp($cur_path, $base_path, strlen($base_path)) !== 0) {
 			return;
+		}
+		if (!file_exists($cur_path) || ($i === count($ns_parts) - 1 ? !is_file($cur_path) : !is_dir($cur_path))) {
+			return;
+		}
+		if ($i === count($ns_parts) - 1) {
+			require $cur_path;
 		}
 	}
 });
