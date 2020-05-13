51eb3a72597f3b295415de2134f9759dc4adf4fb
Alexander Sagen <alexander@sagen.me>
2020-05-13 16:58:38 +0200

    Fix base64url implementation according to RFC 4648

--- inc/encoding/base64_url.php
+++ inc/encoding/base64_url.php
@@ -6,10 +6,10 @@ if (!defined('IN_ZEROX')) {
 
 class Base64Url {
 	public static function encode(string $input) {
-		return str_replace(array('/', '+', '='), array('_', '-', ''), base64_encode($input));
+		return urlencode(str_replace(array('/', '+'), array('_', '-'), base64_encode($input)));
 	}
 
 	public static function decode(string $input) {
-		return base64_decode(str_replace(array('_', '-'), array('/', '+'), $input));
+		return base64_decode(str_replace(array('_', '-'), array('/', '+'), urldecode($input)));
 	}
 }
