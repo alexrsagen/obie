e34d5c1718a86111d467a96a1b32d979400c939c
Alexander Sagen <alexander@sagen.me>
2021-07-15 14:30:53 +0200

    Base64Url: Fix double urlencode

--- encoding/base64_url.php
+++ encoding/base64_url.php
@@ -2,11 +2,11 @@
 
 class Base64Url {
 	public static function encode(string $input) {
-		return urlencode(str_replace(array('/', '+'), array('_', '-'), base64_encode($input)));
+		return str_replace(array('/', '+'), array('_', '-'), base64_encode($input));
 	}
 
 	public static function encodeUnpadded(string $input) {
-		return urlencode(str_replace(array('/', '+'), array('_', '-'), rtrim(base64_encode($input), '=')));
+		return str_replace(array('/', '+'), array('_', '-'), rtrim(base64_encode($input), '='));
 	}
 
 	public static function decode(string $input) {
