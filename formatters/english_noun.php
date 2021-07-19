95032b31582aa4e6068420036768887191bd7f79
Alexander Sagen <alexander@sagen.me>
2021-07-19 13:02:57 +0200

    Formatters\EnglishNoun: Improve plural/singular rules

--- formatters/english_noun.php
+++ formatters/english_noun.php
@@ -1,52 +1,166 @@
 <?php namespace Obie\Formatters;
 
 class EnglishNoun {
-	public static function toSingular(string $name) {
-		if (substr($name, -3) === 'ies') {
-			return substr($name, 0, -3) . 'y';
-		} elseif (substr($name, -2) === 'es') {
-			return substr($name, -2);
-		} elseif (substr($name, -1) === 's' && substr($name, -2, 1) !== 's') {
-			return substr($name, 0, -1);
+	public static $irregular = [
+		'aircraft'     => 'aircraft',
+		'alumnus'      => 'alumni',
+		'analysis'     => 'analyses',
+		'appendix'     => 'appendices', // appendixes
+		'bacterium'    => 'bacteria',
+		'buffalo'      => 'buffalo',
+		'cactus'       => 'cacti',
+		'calf'         => 'calves',
+		'child'        => 'children',
+		'crisis'       => 'crises',
+		'criterion'    => 'criteria',
+		'curriculum'   => 'curricula', // curriculums
+		'datum'        => 'data',
+		'deer'         => 'deer',
+		'die'          => 'dice',
+		'equipment'    => 'equipment',
+		'fish'         => 'fish',
+		'foot'         => 'feet',
+		'fungus'       => 'fungi',
+		'goose'        => 'geese',
+		'hero'         => 'heroes',
+		'hippopotamus' => 'hippopotami', // hippopotamuses
+		'hovercraft'   => 'hovercraft',
+		'index'        => 'indices', // indexes
+		'information'  => 'information',
+		'knife'        => 'knives',
+		'leaf'         => 'leaves',
+		'life'         => 'lives',
+		'man'          => 'men',
+		'memorandum'   => 'memoranda',
+		'money'        => 'money',
+		'moose'        => 'moose',
+		'mouse'        => 'mice',
+		'move'         => 'moves',
+		'nucleus'      => 'nuclei',
+		'octopus'      => 'octopuses', // octopi
+		'ocus'         => 'foci', // focuses
+		'ox'           => 'oxen',
+		'person'       => 'people',
+		'phenomenon'   => 'phenomena',
+		'potato'       => 'potatoes',
+		'radius'       => 'radii', // radiuses
+		'rice'         => 'rice',
+		'series'       => 'series',
+		'sex'          => 'sexes',
+		'sheep'        => 'sheep',
+		'shrimp'       => 'shrimp',
+		'spacecraft'   => 'spacecraft',
+		'species'      => 'species',
+		'stratum'      => 'strata',
+		'swine'        => 'swine',
+		'thesis'       => 'theses',
+		'tomato'       => 'tomatoes',
+		'tooth'        => 'teeth',
+		'torpedo'      => 'torpedoes',
+		'trout'        => 'trout',
+		'valve'        => 'valves',
+		'veto'         => 'vetoes',
+		'vortex'       => 'vortices', // vortexes
+		'watercraft'   => 'watercraft',
+		'wife'         => 'wives',
+		'woman'        => 'women',
+	];
+
+	public static $singular_rules = [
+		'/(quiz)zes$/i'                                                    => '$1',
+		'/(matr)ices$/i'                                                   => '$1ix',
+		'/(vert|ind)ices$/i'                                               => '$1ex',
+		'/^(ox)en$/i'                                                      => '$1',
+		'/(alias)es$/i'                                                    => '$1',
+		'/(octop|vir)i$/i'                                                 => '$1us',
+		'/(cris|ax|test)es$/i'                                             => '$1is',
+		'/(shoe)s$/i'                                                      => '$1',
+		'/(o)es$/i'                                                        => '$1',
+		'/(bus)es$/i'                                                      => '$1',
+		'/([m|l])ice$/i'                                                   => '$1ouse',
+		'/(x|ch|ss|sh)es$/i'                                               => '$1',
+		'/(m)ovies$/i'                                                     => '$1ovie',
+		'/(s)eries$/i'                                                     => '$1eries',
+		'/([^aeiouy]|qu)ies$/i'                                            => '$1y',
+		'/([lr])ves$/i'                                                    => '$1f',
+		'/(tive)s$/i'                                                      => '$1',
+		'/(hive)s$/i'                                                      => '$1',
+		'/(li|wi|kni)ves$/i'                                               => '$1fe',
+		'/(shea|loa|lea|thie)ves$/i'                                       => '$1f',
+		'/(^analy)ses$/i'                                                  => '$1sis',
+		'/((a)naly|(b)a|(d)iagno|(p)arenthe|(p)rogno|(s)ynop|(t)he)ses$/i' => '$1$2sis',
+		'/([ti])a$/i'                                                      => '$1um',
+		'/(n)ews$/i'                                                       => '$1ews',
+		'/(h|bl)ouses$/i'                                                  => '$1ouse',
+		'/(corpse)s$/i'                                                    => '$1',
+		'/(us)es$/i'                                                       => '$1',
+		'/s$/i'                                                            => '',
+	];
+
+	public static $plural_rules = [
+		'/(quiz)$/i'                     => '$1zes',
+		'/^(ox)$/i'                      => '$1en',
+		'/([m|l])ouse$/i'                => '$1ice',
+		'/(matr|vert|ind)ix|ex$/i'       => '$1ices',
+		'/(x|ch|ss|sh)$/i'               => '$1es',
+		'/([^aeiouy]|qu)y$/i'            => '$1ies',
+		'/(hive)$/i'                     => '$1s',
+		'/(?:([^f])fe|([lr])f)$/i'       => '$1$2ves',
+		'/(shea|lea|loa|thie)f$/i'       => '$1ves',
+		'/sis$/i'                        => 'ses',
+		'/([ti])um$/i'                   => '$1a',
+		'/(tomat|potat|ech|her|vet)o$/i' => '$1oes',
+		'/(bu)s$/i'                      => '$1ses',
+		'/(alias)$/i'                    => '$1es',
+		'/(octop)us$/i'                  => '$1i',
+		'/(ax|test)is$/i'                => '$1es',
+		'/(us)$/i'                       => '$1es',
+		'/s$/i'                          => 's',
+		'/$/'                            => 's',
+	];
+
+	public static function toSingular(string $input): string {
+		// check for irregular plural input
+		foreach (static::$irregular as $result => $pattern) {
+			if (substr($pattern, 0, 1) !== '/') {
+				$pattern = '/' . $pattern . '$/i';
+			}
+			if (preg_match($pattern, $input) === 1) {
+				return preg_replace($pattern, $result, $input);
+			}
 		}
-		return $name;
+
+		// match singular regex rules
+		foreach (static::$singular_rules as $pattern => $result) {
+			if (preg_match($pattern, $input) === 1) {
+				return preg_replace($pattern, $result, $input);
+			}
+		}
+
+		// fallback: return unmodified input
+		return $input;
 	}
 
-	public static function toPlural(string $name) {
-		$last_two    = substr($name, -2);
-		$last        = substr($last_two, -1);
-		$second_last = substr($last_two, 0, 1);
-		if ($last === 'y') {
-			if (
-				$second_last === 'a' ||
-				$second_last === 'e' ||
-				$second_last === 'i' ||
-				$second_last === 'o' ||
-				$second_last === 'u'
-			) {
-				return $name . 's';
+	public static function toPlural(string $input): string {
+		// check for irregular singular input
+		foreach (static::$irregular as $pattern => $result) {
+			if (substr($pattern, 0, 1) !== '/') {
+				$pattern = '/' . $pattern . '$/i';
 			}
-			return substr($name, 0, -1) . 'ies';
-		} elseif ($last === 's') {
-			if ($second_last === 'u') {
-				return substr($name, 0, -2) . 'i';
+			if (preg_match($pattern, $input) === 1) {
+				return preg_replace($pattern, $result, $input);
 			}
-			if ($second_last === 'i') {
-				return substr($name, 0, -2) . 'es';
-			}
-			return $name . 'es';
-		} elseif (
-			$last === 'o' ||
-			$last === 'x' ||
-			$last === 'z' ||
-			$last_two === 'sh' ||
-			$last_two === 'ch'
-		) {
-			return $name . 'es';
-		} elseif ($last_two === 'on') {
-			return substr($name, 0, -2) . 'a';
 		}
-		return $name . 's';
+
+		// match plural regex rules
+		foreach (static::$plural_rules as $pattern => $result) {
+			if (preg_match($pattern, $input) === 1) {
+				return preg_replace($pattern, $result, $input);
+			}
+		}
+
+		// fallback: return unmodified input
+		return $input;
 	}
 
 	protected static function getClassName(string $class_name): string {
