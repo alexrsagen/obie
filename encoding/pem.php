a684a61bb1b1ed8ceb40c7b947c91b88158a4cae
Alexander Sagen <alexander@sagen.me>
2022-12-14 22:44:46 +0100

    Pem: Improve types, add new labels

--- encoding/pem.php
+++ encoding/pem.php
@@ -7,45 +7,48 @@ class Pem {
 	const BOUNDARY_PREFIX_POST = '-----END ';
 	const BOUNDARY_SUFFIX      = '-----';
 
-	const LABEL_PUBLICKEY      = 'PUBLIC KEY';
-	const LABEL_RSA_PUBLICKEY  = 'RSA PUBLIC KEY';
-	const LABEL_PGP_PUBLICKEY  = 'PGP PUBLIC KEY BLOCK';
-	const LABEL_PRIVATEKEY     = 'PRIVATE KEY';
-	const LABEL_RSA_PRIVATEKEY = 'RSA PRIVATE KEY';
-	const LABEL_DSA_PRIVATEKEY = 'DSA PRIVATE KEY';
-	const LABEL_EC_PRIVATEKEY  = 'EC PRIVATE KEY';
-	const LABEL_PGP_PRIVATEKEY = 'PGP PRIVATE KEY BLOCK';
-	const LABEL_CERTIFICATE    = 'CERTIFICATE';
-	const LABEL_CSR            = 'CERTIFICATE REQUEST';
-	const LABEL_NEW_CSR        = 'NEW CERTIFICATE REQUEST';
-	const LABEL_PKCS7          = 'PKCS7';
-	const LABEL_X509_CRL       = 'X509 CRL';
+	const LABEL_PUBLICKEY             = 'PUBLIC KEY';
+	const LABEL_RSA_PUBLICKEY         = 'RSA PUBLIC KEY';
+	const LABEL_PGP_PUBLICKEY         = 'PGP PUBLIC KEY BLOCK';
+	const LABEL_PRIVATEKEY            = 'PRIVATE KEY';
+	const LABEL_RSA_PRIVATEKEY        = 'RSA PRIVATE KEY';
+	const LABEL_DSA_PRIVATEKEY        = 'DSA PRIVATE KEY';
+	const LABEL_EC_PRIVATEKEY         = 'EC PRIVATE KEY';
+	const LABEL_PGP_PRIVATEKEY        = 'PGP PRIVATE KEY BLOCK';
+	const LABEL_ENCRYPTED_PRIVATE_KEY = 'ENCRYPTED PRIVATE KEY';
+	const LABEL_CERTIFICATE           = 'CERTIFICATE';
+	const LABEL_ATTRIBUTE_CERTIFICATE = 'ATTRIBUTE CERTIFICATE';
+	const LABEL_CSR                   = 'CERTIFICATE REQUEST';
+	const LABEL_NEW_CSR               = 'NEW CERTIFICATE REQUEST';
+	const LABEL_PKCS7                 = 'PKCS7';
+	const LABEL_X509_CRL              = 'X509 CRL';
+	const LABEL_CMS                   = 'CMS';
 
-	public static function encode(string $data, string $label = self::LABEL_PUBLICKEY) {
+	public static function encode(string $data, string $label = self::LABEL_PUBLICKEY): string {
 		return static::getPreBoundary($label) . "\r\n" .
 			chunk_split(base64_encode($data), 64) .
 			static::getPostBoundary($label);
 	}
 
-	public static function decode(string $data) {
+	public static function decode(string $data): string|false {
 		if (preg_match(self::REGEX, $data, $matches) !== 1) return false;
 		return base64_decode(str_replace([' ', "\t", "\r", "\n"], '', $matches['data']));
 	}
 
-	public static function decodeLabel(string $data) {
+	public static function decodeLabel(string $data): string|false {
 		if (preg_match(self::REGEX, $data, $matches) !== 1) return false;
 		return trim($matches['label'], " \t\r\n");
 	}
 
-	public static function isEncoded(string $data) {
+	public static function isEncoded(string $data): bool {
 		return preg_match(self::REGEX, $data) === 1;
 	}
 
-	public static function getPreBoundary(string $label) {
+	public static function getPreBoundary(string $label): string {
 		return self::BOUNDARY_PREFIX_PRE . strtoupper($label) . self::BOUNDARY_SUFFIX;
 	}
 
-	public static function getPostBoundary(string $label) {
+	public static function getPostBoundary(string $label): string {
 		return self::BOUNDARY_PREFIX_POST . strtoupper($label) . self::BOUNDARY_SUFFIX;
 	}
 }
