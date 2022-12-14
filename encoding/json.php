497bd9c040fb734695fc2f025c466ae827d04507
Alexander Sagen <alexander@sagen.me>
2022-12-14 21:02:29 +0100

    Json: Improve error handling options

--- encoding/json.php
+++ encoding/json.php
@@ -4,9 +4,10 @@ use \Obie\Log;
 class Json {
 	public static function encode($input, int $options = 0): string {
 		try {
-			$output = json_encode($input, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRESERVE_ZERO_FRACTION | JSON_THROW_ON_ERROR | $options);
+			$output = json_encode($input, $options | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRESERVE_ZERO_FRACTION | JSON_THROW_ON_ERROR);
 			return $output;
 		} catch (\JsonException $e) {
+			if ($options & JSON_THROW_ON_ERROR) throw $e;
 			Log::error($e->getMessage(), ['prefix' => 'JSON encode error:']);
 			Log::debug($e->getTraceAsString(), ['prefix' => 'Stack trace:']);
 			return '';
@@ -18,6 +19,7 @@ class Json {
 			$output = json_decode($input, $assoc, $depth, $options | JSON_THROW_ON_ERROR);
 			return $output;
 		} catch (\JsonException $e) {
+			if ($options & JSON_THROW_ON_ERROR) throw $e;
 			Log::error($e->getMessage(), ['prefix' => 'JSON decode error:']);
 			Log::debug($e->getTraceAsString(), ['prefix' => 'Stack trace:']);
 			return null;
