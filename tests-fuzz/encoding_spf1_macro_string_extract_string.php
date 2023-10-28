2c8748208e3c2257c349a8aa7879f8f51208c0f5
Alexander Sagen <alexander@sagen.me>
2023-10-29 01:36:45 +0200

    Add fuzz tests for most if not all parsers

---
+++ tests-fuzz/encoding_spf1_macro_string_extract_string.php
@@ -0,0 +1,12 @@
+<?php
+/** @var PhpFuzzer\Config $config */
+
+require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'autoload.php';
+
+$config->setTarget(function (string $input) {
+	$pos = 0;
+    Obie\Encoding\Spf1\MacroString::extractString($input, $pos);
+});
+
+$config->setMaxLen(512);
---
+++ tests-fuzz/http_multipart_form_data_decode.php
@@ -0,0 +1,11 @@
+<?php
+/** @var PhpFuzzer\Config $config */
+
+require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'autoload.php';
+
+$config->setTarget(function (string $input) {
+    Obie\Http\Multipart\FormData::decode($input);
+});
+
+$config->setMaxLen(512);
